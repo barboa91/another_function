c18895013eded1cd94be70ade5fbaec65a5b75e4
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2023-07-22 14:37:33 -0400

    some functions for basic syntax practice.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,19 @@ use rand::Rng;
 
 fn main() {
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // original_main();
+    f_to_c();
+    //  ownership();
+
+
+}
+
+fn ownership() {
+    let s ="hello";
+}
+
+fn original_main() {
+        let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number!");
     println!("The number is {}!", secret_number);
     
@@ -30,6 +42,32 @@ fn main() {
             Ordering::Equal => {println!("Jussstt Right");break;}
         }
     }
+}
 
-    
+fn f_to_c(){
+
+    println!("Enter a temp in fahrenhite to convert it to celsius");
+
+    let mut fah = String::new();
+
+        loop{
+            io::stdin()
+                .read_line(&mut fah)
+                .expect("Failed to read in");
+
+            let  x: u32  = match fah.trim().parse(){
+                Ok(fart) => fart,
+                Err(e) => {{println!("{}",e)};continue},
+            };
+
+            let xCel = ((x as f32) -32.0)/1.6;
+            
+            // };
+            println!("{x} Fahernheit is equal to {xCel}");  
+
+            break;
+
+        }
+
+
 }
