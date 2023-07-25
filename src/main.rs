47c9a2b3a4bb673ca211408524d6ac58bbf5e831
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2023-07-24 22:37:03 -0400

    chapter 4

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ use rand::Rng;
 fn main() {
 
     // original_main();
-    f_to_c();
+    // f_to_c();
     //  ownership();
+    // fibo(10);
+    randomtesting();
 
 
-}
 
-fn ownership() {
-    let s ="hello";
 }
 
 fn original_main() {
@@ -20,7 +19,7 @@ fn original_main() {
     println!("Guess the number!");
     println!("The number is {}!", secret_number);
     
-    loop{
+    loop{ 
         println!("enter your guess");
 
         let mut guess = String::new();
@@ -69,5 +68,67 @@ fn f_to_c(){
 
         }
 
+}
+
+fn fibo(n_fib : u32){
+    let mut fib = String::new();
+
+    println!("Get the nth number in fibo sequence");
+    
+
+    io::stdin()
+        .read_line(&mut fib)
+        .expect("didnt get the read");
+
+
+
+    println!("{n_fib}");
+}
+
+fn get_input() -> String{
+
+    let mut inp = String::new();
+
+    io::stdin()
+    .read_line(&mut inp)
+    .expect("didnt get the read");
+
+
+    return inp;
+
+}
+
+fn randomtesting() {
+
+    let mut s = String::from("hello world");
+    println!("{} <--, this is s",s);
+
+
+    let word = first_word(&s); // word will get the value 5
+    println!("{} <--, this is word",word);
+
+    s = "farts".to_string();
+    // s.clear(); // this empties the String, making it equal to ""
+    println!("clearing s");
+
+    println!("{} <--, this is s",s);
+    // println!("{} <--, this is word",word);
+
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 5 with. word is now totally invalid!
+
+
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 
 }
